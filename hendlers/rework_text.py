663ef060a811505cda70282f252ca99aa8965a8c
034qwe <bnestercuk91@gmail.com>
2022-12-23 20:35:48 +0200

    no idea

--- hendlers/rework_text.py
+++ hendlers/rework_text.py
@@ -9,13 +9,14 @@ headers = {
             }
 async def rework_text(message:types.Message):
     text = message.text.replace('/rework','')
-    url ="https://translate.google.com/?hl=ru&sl=ru&tl=en&text=%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D1%82%0A%0A&op=translate"
+    url ="https://translate.google.com/?hl=ru&sl=auto&tl=ru&text=%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D1%82&op=translate"
     r = requests.get(url=url,headers=headers)
     soup = BeautifulSoup(r.text,'lxml')
-    a = soup.find('div',class_='lRu31').find('span')
-    await message.answer(a)
+    a = soup.find('div',class_='QcsUad BDJ8fb')
     print(a)
+    await message.answer('1234')
 
+    
 
 def register_handlers_rework(dp:Dispatcher):
     dp.register_message_handler(rework_text,commands=['rework'])
