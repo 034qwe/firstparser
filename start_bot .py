804180a492c363402d3ef1746a6e4f38452b6c14
034qwe <bnestercuk91@gmail.com>
2022-12-28 19:31:47 +0200

    add new func in start def

--- start_bot .py
+++ start_bot .py
@@ -2,6 +2,8 @@ from aiogram.utils import executor
 from create_bot import dp
 from hendlers import other, wiki,rework_text
 from keyboards import lang_kb
+from selenium_folder import main_selenium
+
 
 async def on_startup(_):
     print('bot start')
@@ -10,7 +12,7 @@ async def on_startup(_):
 other.register_handlers_other(dp)
 wiki.register_handlers_maintext(dp)
 lang_kb.register_handlers_langkb(dp)
-rework_text.register_handlers_rework(dp)
+main_selenium.register_handlers_selenium(dp)
 
 
 executor.start_polling(dp, skip_updates=True,on_startup=on_startup)
