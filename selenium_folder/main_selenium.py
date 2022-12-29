7d22f97cb1f7ec795c8ba0c88b3fb702402858fb
034qwe <bnestercuk91@gmail.com>
2022-12-29 19:07:45 +0200

    finish main func

--- selenium_folder/main_selenium.py
+++ selenium_folder/main_selenium.py
@@ -12,18 +12,30 @@ headers = {
 
 async def r_text(message:types.Message):
     text = message.text.replace('/rework','')
-    url = f'https://www.deepl.com/ru/translator#uk/en/{text}'
+
 
     try:
+        url = f'https://www.deepl.com/ru/translator#ru/zh/{text}'
+
         driver = webdriver.Chrome(executable_path="D:/programer/projects/firstparser-main/firstparser-main/firstparser\selenium/chromedriver.exe")
 
         driver.get(url=url)
-        time.sleep(1)
-
+        time.sleep(5)
 
         soup = BeautifulSoup(driver.page_source,'lxml')
         a = soup.find('div',class_='lmt__textarea lmt__textarea_dummydiv',id='target-dummydiv')
-        await message.answer(a.text)
+
+
+        url2=f'https://www.deepl.com/ru/translator#zh/uk/{a.text}'
+                
+        driver.get(url=url2)
+        time.sleep(5)
+
+        soup = BeautifulSoup(driver.page_source,'lxml')
+        answ = soup.find('div',class_='lmt__textarea lmt__textarea_dummydiv',id='target-dummydiv')
+
+
+        await message.answer(answ.text)
 
     except:
         print('bad')
